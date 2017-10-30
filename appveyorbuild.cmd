1849253035ac6c4eae56339b2ad99422e15940a1
Tyler Richey <iam@tylerrichey.com>
2017-10-30 13:31:55 -0400

    woo

--- appveyorbuild.cmd
+++ appveyorbuild.cmd
@@ -2,7 +2,10 @@
 set DOTNET_CLI_TELEMETRY_OPTOUT=1
 dotnet restore -v Minimal
 cd DarkStatsCore 
+setlocal
 call npm install
+endlocal
+dir node_modules
 dotnet publish -r win10-x64 -o .\darkstatscore-win10-x64
 dotnet publish -r osx.10.12-x64 -o .\darkstatscore-osx.10.12-x64
 dotnet publish -r debian.8-x64 -o .\darkstatscore-debian.8-x64
