d948b3d3e292f3a766c33f77e3b72e5701370e02
Tyler Richey <iam@tylerrichey.com>
2017-10-30 08:08:44 -0400

    re-factor scrape + build changes

---
+++ DarkStatsCore.Data/Models/TrafficCache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DarkStatsCore.Data.Models
+{
+    public class TrafficCache
+    {
+        public string Ip { get; set; }
+        public long In { get; set; }
+        public long Out { get; set; }
+        public int HourAdded { get; set; }
+    }
+}
--- DarkStatsCore/copypackages.js
+++ DarkStatsCore/copypackages.js
@@ -1,11 +1,10 @@
-var csv = require("fast-csv");
-var copyfiles = require('copyfiles');
- 
-csv
- .fromPath("copypackages.csv")
- .on("data", function(data){
-	 copyfiles(data, [], function(){});
- })
- .on("end", function(){
-     console.log("Copy packages completed.");
- });
+var copyfiles = require("copyfiles");
+var packages = require("./copypackages.files.json");
+
+packages.forEach(function (files) {
+	copyfiles(files, [], function(){});
+	console.log("Copied " + files[0] + " to " + files[1]);
+});
+
+console.log("Copy packages to wwwroot completed.");
+console.log();
