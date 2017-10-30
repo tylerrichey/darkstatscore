423f7c15e90512f1f6c05afb8dcd1b7dee297cda
Tyler Richey <iam@tylerrichey.com>
2017-10-30 10:33:42 -0400

    ring around the npm

--- appveyorbuild.cmd
+++ appveyorbuild.cmd
@@ -2,7 +2,7 @@
 set DOTNET_CLI_TELEMETRY_OPTOUT=1
 dotnet restore -v Minimal
 cd DarkStatsCore 
-npm -v
+npm install -g npm
 npm install
 dotnet publish -r win10-x64 -o .\darkstatscore-win10-x64
 dotnet publish -r osx.10.12-x64 -o .\darkstatscore-osx.10.12-x64
